b7e11f2e6ec6c4e5a47bac53ae627d4550e8dd4a
Trevor Esparza <trevoresparza30@gmail.com>
2022-03-01 19:30:11 -0600

    java

---
+++ src/main/ConsoleExercises.java
@@ -0,0 +1,86 @@
+package main;
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+//        String greeting = "Salutations";
+//        String name = "codeup";
+//        System.out.printf("%s, %s! \n", greeting, name);
+//
+//        Scanner myDemoScanner = new Scanner(System.in);
+//        System.out.print("Enter something: ");
+//        String userInput = myDemoScanner.nextLine();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+//        double pi = 3.14159;
+//        System.out.printf("The value of PI is approximately %s \n" , pi);
+//        System.out.printf("The value of PI is approximately %.2f \n" , pi);
+//        System.out.printf("The value of PI is approximately %.3f \n" , pi);
+//        System.out.printf("The value of PI is approximately %.4f \n" , pi);
+//
+//
+//        Scanner myDemoScanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int userInput = myDemoScanner.nextInt();
+//        System.out.println("You entered the integer: --> \"" + userInput + "\" <--");
+//
+//        Scanner mySecondDemoScanner = new Scanner(System.in);
+//        System.out.print("Please enter three strings:");
+//        String firstUserInput = mySecondDemoScanner.next();
+//        String secondUserInput = mySecondDemoScanner.next();
+//        String thirdUserInput = mySecondDemoScanner.next();
+//        System.out.printf("You entered the following strings:\n %s \n %s \n %s \n", firstUserInput,secondUserInput,thirdUserInput);
+//
+//        Scanner myThirdDemoScanner = new Scanner(System.in);
+//        System.out.print("Please enter an entire sentence:");
+//        String userInputSentence = myThirdDemoScanner.nextLine();
+//        System.out.printf("You typed the following sentence: %s \n", userInputSentence);
+
+//        Scanner myFourthDemoScanner = new Scanner(System.in);
+//        System.out.print("Please enter a length: ");
+//        int roomLength = myFourthDemoScanner.nextInt();
+//        System.out.print("Please enter a width: ");
+//        int roomWidth = myFourthDemoScanner.nextInt();
+//        System.out.printf("You entered the following dimensions: W: %s, L: %s \n",roomWidth,roomLength);
+//        int sqfootage = roomLength * roomWidth;
+//        System.out.printf("You're total area is: %s  sqft! \n", sqfootage);
+//        System.out.print("Please enter a ceiling height: ");
+//        int ceilingHeight = myFourthDemoScanner.nextInt();
+//        int totalRoomVolume = roomLength * roomWidth * ceilingHeight;
+//        System.out.printf("Your room volume is: %s cuft \n \n", totalRoomVolume);
+
+//        Scanner myFifthDemoScanner = new Scanner(System.in);
+//        System.out.print("Please enter a length: ");
+//        float roomLengthFloat = myFifthDemoScanner.nextFloat();
+//        System.out.print("Please enter a width: ");
+//        float roomWidthFloat = myFifthDemoScanner.nextFloat();
+//        System.out.printf("You entered the following dimensions: W: %s, L: %s \n",roomWidthFloat,roomLengthFloat);
+//        float sqfootageFloat = roomLengthFloat * roomWidthFloat;
+//        System.out.printf("You're total area is: %s  sqft! \n", sqfootageFloat);
+//        System.out.print("Please enter a ceiling height: ");
+//        float ceilingHeightFloat = myFifthDemoScanner.nextFloat();
+//        float totalRoomVolumeFloat = roomLengthFloat * roomWidthFloat * ceilingHeightFloat;
+//        System.out.printf("Your room volume is: %s cuft", totalRoomVolumeFloat);
+
+        Scanner mySixthDemoScanner = new Scanner(System.in);
+        mySixthDemoScanner.useDelimiter("/\n");
+        System.out.print("Please enter Length:");
+        int lengthSixthDemo = mySixthDemoScanner.nextInt();
+        System.out.print("Please enter Width:");
+        int widthSixthDemo = mySixthDemoScanner.nextInt();
+        System.out.print("Please enter Height:");
+        int heightSixthDemo = mySixthDemoScanner.nextInt();
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,8 +1,63 @@
 package main;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("Hello");
+        System.out.print(" World!");
+        System.out.print("\n");
+        int myAge = 45;
+        System.out.println(myAge);
+        int myFavoriteNumber = 12;
+        String myString = "codeup";
+        System.out.println(myString);
+        myString = "san antonio";
+        System.out.println(myString);
+        myString = String.valueOf('c');
+        System.out.println(myString);
+        myFavoriteNumber = (int) 3.14;
+        System.out.println(myFavoriteNumber);
+        myFavoriteNumber = 123;
+        float myNumber = (float) 3.14;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int j = 4;
+        j += 4;
+        System.out.println(j);
+
+        int l = 3;
+        int m = 4;
+        l *= m;
+        System.out.println(l);
+
+        int d = 10;
+        int e = 2;
+        d /= e;
+        e -= d;
+        System.out.println(d);
+        System.out.println(e);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+
+
+
+
     }
 }
 
