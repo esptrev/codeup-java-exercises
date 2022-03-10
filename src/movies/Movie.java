1e9adcab6820999812adb96869de3492248fac7c
Trevor Esparza <trevoresparza30@gmail.com>
2022-03-10 16:02:24 -0600

    arrays

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,23 @@
 package movies;
 
+
 public class Movie {
+    private String name;
+    private String category;
+
+
+    public Movie(String movieName, String movieCategory) {
+        this.name = movieName;
+        this.category = movieCategory;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
